2db20fd7af8ed68d4ce474415a43b755721e8954
SparshJain <jainsparsh17@gmail.com>
2024-04-21 17:48:44 +0530

    added medium logo and title for the landing signin and signup page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,10 +1,15 @@
 import { Auth } from "../components/Auth";
 import { Quote } from "../components/Quote";
+import logo from "../assets/medium.png"
 
 export const Signin = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2">
       <div>
+        <div className="flex gap-2 items-center">
+        <img className="w-8 h-8 mt-2 ml-2" src={logo} alt="" />
+          <span className="text-2xl font-bold font-mono mt-2">Medium</span>
+        </div>
         <Auth type="signin" />
       </div>
       <div>
